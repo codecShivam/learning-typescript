317b4cdb51a7a842df5ea8d8e2c2dc347aef6927
codecShivam <codecshivam@gmail.com>
2023-08-25 19:27:21 +0530

    chapter 1: data types , config files

---
+++ LessonNo1/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let username = "shivam";
+console.log(username);
+let a = 10;
+let b = '20';
+let c = 2;
+console.log(a + b);
+console.log(a / b);
+console.log(b * c);
---
+++ LessonNo1/src/main.ts
@@ -0,0 +1,9 @@
+let username = "shivam";
+console.log(username);
+
+let a: number = 10;
+let b: string = '20';
+let c: number = 2;
+console.log(a + b);
+console.log(a / b);
+console.log(b * c);

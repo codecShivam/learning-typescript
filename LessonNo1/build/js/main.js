26410e475a3e57334f2f14b9696b67837cfb0462
codecShivam <codecshivam@gmail.com>
2023-08-26 15:58:42 +0530

    chapter 2

--- LessonNo1/build/js/main.js
+++ LessonNo1/build/js/main.js
@@ -1,9 +1,29 @@
 "use strict";
-let username = "shivam";
-console.log(username);
-let a = 10;
-let b = '20';
-let c = 2;
-console.log(a + b);
-console.log(a / b);
-console.log(b * c);
+// let username = "shivam";
+// console.log(username);
+// let a: number = 10;
+// let b: string = '20';
+// let c: number = 2;
+// console.log(a + b);
+// console.log(a / b);
+// console.log(b * c);
+let myName;
+myName = "Shivam";
+;
+myName = "Shivam Yadav";
+let meaningOfLife;
+let isLoading;
+isLoading = true;
+meaningOfLife = 42;
+let album;
+let unionType; //can be number or string
+album = 2453;
+album = "Shivam";
+album = true;
+const sum = (a, b) => {
+    return a + b;
+};
+sum(10, 20);
+let postId;
+let isActive;
+let re = /\w+/g;
--- LessonNo1/src/main.ts
+++ LessonNo1/src/main.ts
@@ -1,9 +1,35 @@
-let username = "shivam";
-console.log(username);
+// let username = "shivam";
+// console.log(username);
 
-let a: number = 10;
-let b: string = '20';
-let c: number = 2;
-console.log(a + b);
-console.log(a / b);
-console.log(b * c);
+// let a: number = 10;
+// let b: string = '20';
+// let c: number = 2;
+// console.log(a + b);
+// console.log(a / b);
+// console.log(b * c);
+
+let myName: String;
+myName = "Shivam";;
+myName = "Shivam Yadav";
+let meaningOfLife: number ;
+let isLoading: boolean;
+
+isLoading = true;
+meaningOfLife = 42;
+let album: any;
+let unionType: number | string; //can be number or string
+
+album = 2453;
+album = "Shivam";
+album = true;   
+
+const sum = (a: number, b: number) => {
+    return a + b;
+}
+
+sum(10, 20);
+
+let postId: string | number;
+let isActive: number | boolean | string;
+
+let re: RegExp = /\w+/g;

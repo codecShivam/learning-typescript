10ccbb1613ced455fcf5fa3acff6323fa69d9db6
codecShivam <codecshivam@gmail.com>
2023-08-27 14:56:16 +0530

    chp 3:arr,obj,tuples,enum,interface,type

--- LessonNo1/build/js/main.js
+++ LessonNo1/build/js/main.js
@@ -7,23 +7,110 @@
 // console.log(a + b);
 // console.log(a / b);
 // console.log(b * c);
-let myName;
-myName = "Shivam";
-;
-myName = "Shivam Yadav";
-let meaningOfLife;
-let isLoading;
-isLoading = true;
-meaningOfLife = 42;
-let album;
-let unionType; //can be number or string
-album = 2453;
-album = "Shivam";
-album = true;
-const sum = (a, b) => {
-    return a + b;
+// let myName: String;
+// myName = "Shivam";;
+// myName = "Shivam Yadav";
+// let meaningOfLife: number ;
+// let isLoading: boolean;
+// isLoading = true;
+// meaningOfLife = 42;
+// let album: any;
+// let unionType: number | string; //can be number or string
+// album = 2453;
+// album = "Shivam";
+// album = true;   
+// const sum = (a: number, b: number) => {
+//     return a + b;
+// }
+// sum(10, 20);
+// let postId: string | number;
+// let isActive: number | boolean | string;
+// let re: RegExp = /\w+/g;
+let stringArr = ['one', 'hey', 'shivam'];
+let guitars = ['Fender', 'Gibson', 'Ibanez', 'Taylor', 23];
+let mixedData = ['evh', 687, true];
+stringArr[0] = 'two';
+stringArr.push('three');
+guitars[0] = 545; //no problem switching to number type as it is union type array 
+guitars.push('Yamaha');
+guitars.push(545);
+// guitars.push(true); //error as it is not union type array 
+// stringArr = guitars; //error as it is not union type array
+guitars = stringArr; //no error as it is union type array
+// guitars = mixedData; //error as it is not union type array
+mixedData = guitars; //no error as it is union type array
+let test = []; //any type array
+test.push(1);
+test.push('shivam');
+test.push(true);
+let test2 = []; //string type array
+test2.push('shivam');
+// tuple 
+let myTuple = ['hey', 23, true]; //tuple type array
+let mixed = ['hey', 23, true]; //union type array
+mixed = myTuple;
+// myTuple = mixed //Type '(string | number | boolean)[]' is not assignable to type '[string, number, boolean]'.
+//   Target requires 3 element(s) but source may have fewer.
+// myTuple[3] = 24 
+myTuple[1] = 23; //only 3 elements are allowed in tuple and type of elements should be same as defined in tuple 
+let myObj;
+myObj = []; //no error as array is also an object
+myObj = test2;
+myObj = myTuple;
+myObj = mixed;
+myObj = {};
+const exampleObj = {
+    prop1: 'hey',
+    prop2: true,
 };
-sum(10, 20);
-let postId;
-let isActive;
-let re = /\w+/g;
+let guitar;
+guitar = {
+    name: 'Fender',
+    active: true,
+    albums: [1, 2, 3, 'ytfy']
+};
+let guitar2;
+guitar2 = {
+    name: 'Fender',
+    active: true,
+    albums: [1, 2, 3, 'ytfy']
+};
+guitar = guitar2; //no error as both are of same type and have same properties
+let guitar3;
+guitar3 = {
+    name: 'Fender',
+    albums: [1, 2, 3, 'ytfy']
+};
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.name}!`;
+};
+console.log(greetGuitarist(guitar));
+//enum
+// unlike most typecript features, enums are not a type-level addition to javascript. Instead, they are compile-time constructs that are compiled to javascript objects. something added to the language and runtime environment.
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 0] = "U";
+    Grade[Grade["D"] = 1] = "D";
+    Grade[Grade["C"] = 2] = "C";
+    Grade[Grade["B"] = 3] = "B";
+    Grade[Grade["A"] = 4] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.C); //2 only if we will not provide value to enum then it will give index of that element 
+console.log(Grade[2]); //C  
+var Grade2;
+(function (Grade2) {
+    Grade2["U"] = "U";
+    Grade2["D"] = "D";
+    Grade2["C"] = "C";
+    Grade2["B"] = "B";
+    Grade2["A"] = "A";
+})(Grade2 || (Grade2 = {}));
+console.log(Grade2.C); //C
+// console.log(Grade2[2]); //undefined
+var Grade3;
+(function (Grade3) {
+    Grade3[Grade3["U"] = 2] = "U";
+    Grade3[Grade3["D"] = 3] = "D";
+    Grade3[Grade3["C"] = 4] = "C";
+})(Grade3 || (Grade3 = {}));
+console.log(Grade3.C); //4 as it will start from 2 and increment by 1 
--- LessonNo1/src/main.ts
+++ LessonNo1/src/main.ts
@@ -8,28 +8,162 @@
 // console.log(a / b);
 // console.log(b * c);
 
-let myName: String;
-myName = "Shivam";;
-myName = "Shivam Yadav";
-let meaningOfLife: number ;
-let isLoading: boolean;
+// let myName: String;
+// myName = "Shivam";;
+// myName = "Shivam Yadav";
+// let meaningOfLife: number ;
+// let isLoading: boolean;
 
-isLoading = true;
-meaningOfLife = 42;
-let album: any;
-let unionType: number | string; //can be number or string
+// isLoading = true;
+// meaningOfLife = 42;
+// let album: any;
+// let unionType: number | string; //can be number or string
 
-album = 2453;
-album = "Shivam";
-album = true;   
+// album = 2453;
+// album = "Shivam";
+// album = true;   
 
-const sum = (a: number, b: number) => {
-    return a + b;
+// const sum = (a: number, b: number) => {
+//     return a + b;
+// }
+
+// sum(10, 20);
+
+// let postId: string | number;
+// let isActive: number | boolean | string;
+
+// let re: RegExp = /\w+/g;
+
+let stringArr = ['one','hey','shivam']
+
+let guitars = ['Fender', 'Gibson', 'Ibanez', 'Taylor',23];
+
+let mixedData = [ 'evh', 687,true]
+
+stringArr[0] = 'two';
+stringArr.push('three');
+
+guitars[0] = 545; //no problem switching to number type as it is union type array 
+guitars.push('Yamaha');
+guitars.push(545);
+// guitars.push(true); //error as it is not union type array 
+
+// stringArr = guitars; //error as it is not union type array
+guitars = stringArr; //no error as it is union type array
+
+// guitars = mixedData; //error as it is not union type array
+mixedData = guitars; //no error as it is union type array
+
+let test = []; //any type array
+test.push(1);
+test.push('shivam');
+test.push(true);
+
+let test2: string[] = []; //string type array
+test2.push('shivam');
+
+// tuple 
+let myTuple: [string, number, boolean] = ['hey', 23, true]; //tuple type array
+
+let mixed = ['hey', 23, true]; //union type array
+
+mixed = myTuple 
+
+// myTuple = mixed //Type '(string | number | boolean)[]' is not assignable to type '[string, number, boolean]'.
+//   Target requires 3 element(s) but source may have fewer.
+
+// myTuple[3] = 24 
+myTuple[1] = 23; //only 3 elements are allowed in tuple and type of elements should be same as defined in tuple 
+
+let myObj: object;
+myObj = []; //no error as array is also an object
+myObj = test2;
+myObj = myTuple;
+myObj = mixed;
+
+myObj = {};
+
+const exampleObj = {
+    prop1: 'hey',
+    prop2: true,
 }
 
-sum(10, 20);
+// exampleObj.prop2 =  23; 
 
-let postId: string | number;
-let isActive: number | boolean | string;
+// type Guitars = {
+//     name: string,   
+//     active: boolean,
+//     albums: (string | number)[]
+// }
 
-let re: RegExp = /\w+/g;
+
+interface Guitars {  
+    name: string,
+    active: boolean,
+    albums: (string | number)[]
+} //same as type Guitars it works same as type Guitars
+
+let guitar: Guitars;
+guitar = {
+    name: 'Fender',
+    active: true,//if you remove this property then it will give error as it is required
+    albums: [1,2,3,'ytfy']
+}
+
+let guitar2: Guitars;
+guitar2 = {
+    name: 'Fender', 
+    active: true,
+    albums: [1,2,3,'ytfy']
+}
+
+guitar = guitar2; //no error as both are of same type and have same properties
+
+type Guitars2 = {
+    name: string,
+    active?: boolean,//? means optional property if we will not provide this property then also it will not give error it can be either boolean or undefined
+    albums: (string | number)[]
+}
+
+let guitar3: Guitars2;
+guitar3 = {
+    name: 'Fender',
+    albums: [1,2,3,'ytfy']
+}
+
+const greetGuitarist = (guitarist: Guitars) => {
+    return `Hello ${guitarist.name}!`;
+}
+console.log(greetGuitarist(guitar));
+
+//enum
+// unlike most typecript features, enums are not a type-level addition to javascript. Instead, they are compile-time constructs that are compiled to javascript objects. something added to the language and runtime environment.
+
+enum Grade {
+    U,
+    D,
+    C,
+    B,
+    A
+}
+
+console.log(Grade.C); //2 only if we will not provide value to enum then it will give index of that element 
+console.log(Grade[2]); //C  
+
+enum Grade2 {
+    U = 'U',
+    D = 'D',
+    C = 'C',
+    B = 'B',
+    A = 'A'
+}
+console.log(Grade2.C); //C
+// console.log(Grade2[2]); //undefined
+
+enum Grade3 {
+    U=2,
+    D,
+    C,
+}
+
+console.log(Grade3.C); //4 as it will start from 2 and increment by 1 
